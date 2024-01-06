a1c51c41b099a2cbfa0866b09a94d7a2066589d5
Romane Orlando Robb <mail@romanerobb.com>
2024-01-06 12:18:40 -0500

    Updated.

---
+++ app.js
@@ -0,0 +1,25 @@
+// nav toggler
+
+let toggleBtn = document.querySelector('.nav-toggler');
+let linksContainer = document.querySelector('.links-container');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    linksContainer.classList.toggle('active');
+})
+
+// faq
+
+let faqs = [...document.querySelectorAll('.faq')];
+
+faqs.map( faq => {
+
+    let ques = faq.querySelector('.question-box');
+
+    ques.addEventListener('click', () => {
+        faq.classList.toggle('active');
+    })
+
+})
+
+AOS.init();
